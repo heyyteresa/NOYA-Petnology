d3e24b0f4f4b82e9cbac602a207aa4a379716688
yifenyifeng <archevanliu@gmail.com>
2025-07-14 18:41:21 -0700

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/NOYA-Petnology/', // 使用你的仓库名
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
